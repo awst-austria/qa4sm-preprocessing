4ac17cc7b71f14b66bb3106dab462e26db42dbf1
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2024-11-10 13:21:28 +0100

    Revert default variable

--- src/qa4sm_preprocessing/level2/smos.py
+++ src/qa4sm_preprocessing/level2/smos.py
@@ -30,7 +30,6 @@ class SMOSL2Reader(L2Reader):
                 "Science_Flags",
                 "Confidence_Flags",
                 "Processing_Flags",
-                "Chi_2_P",
                 "Chi_2",
                 "RFI_Prob",
                 "N_RFI_X",
